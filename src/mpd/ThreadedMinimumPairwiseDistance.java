5304eff03208885433b71ac5ac541399e7b4bda8
Philip Blaskowski <blask017@morris.umn.edu>
2017-11-20 20:56:58 -0600

    Finish ThreadedMinimumPairwiseDistance.java all tests pass, including the timing test

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,12 +1,25 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
-	private int minimum = Integer.MAX_VALUE;
+	public int minimum = Integer.MAX_VALUE;
 
 	@Override
 	public int minimumPairwiseDistance(int[] values) {
-		RangeSplitter lowerLeft = new RangeSplitter(values, 0, values.length/2);
-		RangeSplitter bottomRight = new RangeSplitter(values, values.length/2, values.length);
+		RangeSplitter[] splitters = new RangeSplitter[4];
+
+		for (int i = 0; i < 4; i++) {
+			splitters[i] = new RangeSplitter(values, i);
+			splitters[i].start();
+		}
+
+		for (int i = 0; i < 4; i++) {
+			try {
+				splitters[i].join();
+			} catch (InterruptedException e) {
+				System.out.println("oops something wierd happened");
+				e.printStackTrace();
+			}
+		}
 		return minimum;
 	}
 
@@ -16,28 +29,86 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 		}
 	}
 
-	public class RangeSplitter extends Thread {
-		private int minimum = Integer.MAX_VALUE;
-		private int[] arr;
-		private int start;
-		private int end;
+	private class RangeSplitter extends Thread {
+		private int[] values;
+		private int length;
+		private int mode;
 
-		public RangeSplitter(int[] arr, int i, int j) {
-			this.arr = arr;
-			if(j < i){
-				this.start = j;
-				this.end = i;
-			}else{
-				this.start = i;
-				this.end = j;
+		public RangeSplitter(int[] arr, int mode) {
+			this.values = arr;
+			this.length = arr.length;
+			this.mode = mode;
+		}
+
+		private int lowerLeft() {
+			int result = Integer.MAX_VALUE;
+			for (int i = 1; i < this.length / 2; i++) {
+				for (int j = 0; j < i; j++) {
+					int diff = Math.abs(this.values[i] - this.values[j]);
+					if(diff < result){
+						result = diff;
+					}
+				}
 			}
+			
+			return result;
+		}
+
+		private int bottomRight() {
+			int result = Integer.MAX_VALUE;
+			for (int i = this.length / 2 + 1; i < this.length; i++) {
+				for (int j = 0; j + this.length / 2 < i; j++) {
+					int diff = Math.abs(this.values[i] - this.values[j]);
+					if(diff < result){
+						result = diff;
+					}
+				}
+			}
+			
+			return result;
+		}
+
+		private int topRight() {
+			int result = Integer.MAX_VALUE;
+			for (int i = this.length / 2 + 1; i < this.length; i++) {
+				for (int j = this.length / 2; j < i; j++) {
+					int diff = Math.abs(this.values[i] - this.values[j]);
+					if(diff < result){
+						result = diff;
+					}
+				}
+			}
+			
+			return result;
+		}
+
+		private int center() {
+			int result = Integer.MAX_VALUE;
+			for (int j = 0; j + this.length / 2 < this.length; j++) {
+				for (int i = this.length / 2; i <= j + this.length/2; i++) {
+					int diff = Math.abs(this.values[i] - this.values[j]);
+					if(diff < result){
+						result = diff;
+					}
+				}
+			}
+			
+			return result;
 		}
 
 		public void run() {
-			for (int i = this.start; i < this.end; i++) {
-				for (int j = this.start; j < this.end; j++) {
-					updateMinimum(Math.abs(arr[i] - arr[j]));
-				}
+			if (this.mode == 0) {
+				System.out.println("starting lower left");
+				updateMinimum(lowerLeft());
+			} else if (this.mode == 1) {
+				System.out.println("starting bottom right");
+				updateMinimum(bottomRight());
+			} else if (this.mode == 2) {
+				System.out.println("starting top right");
+				updateMinimum(topRight());
+			} else {
+				System.out.println("starting center");
+				updateMinimum(center());
 			}
 		}
 	}
